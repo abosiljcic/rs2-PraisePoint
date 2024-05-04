0ac34803c33d704e33299b1f71386c1604b9bc05
Andrijana <andrijanab2000@gmail.com>
2024-05-04 17:52:07 +0200

    New: Added User entity class

---
+++ PraisePoint/Services/User/User.API/Entities/User.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace User.API.Entities
+{
+    public class User : IdentityUser<int>
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Role { get; set; }
+        public int CompanyId { get; set; }
+        public string ImageUrl { get; set; }
+        public bool Active { get; set; }
+    }
+}
