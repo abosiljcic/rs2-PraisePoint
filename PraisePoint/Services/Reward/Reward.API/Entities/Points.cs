7cf99f99f829f56f60dbd97523e5e9f10722783e
KatMilosevic <milosevickatarina626@gmail.com>
2024-05-08 15:38:11 +0200

    Added Points Entity

---
+++ PraisePoint/Services/Reward/Reward.API/Entities/Points.cs
@@ -0,0 +1,10 @@
+﻿namespace Reward.API.Entities
+{
+    public class Points
+    {
+        public string user_id { get; set; }
+        public int received_points { get; set; }
+        public int budget { get; set; }
+        public string company_id { get; set; }
+    }
+}
