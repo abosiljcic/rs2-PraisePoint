921b1d2eea46d1a71a53e622c5e740245c1b35eb
nickla <ninalazarevic37@icloud.com>
2024-05-11 17:22:00 +0200

    New: Start implementation for application layer for post service

---
+++ PraisePoint/Services/Post/Post.Application/Contracts/Persistence/IPostRepository.cs
@@ -0,0 +1,17 @@
+﻿using Post.Domain.Aggregates;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Post.Application.Contracts.Persistence
+{
+    public interface IPostRepository : IAsyncRepository<Domain.Aggregates.Post>
+    {
+        Task<IReadOnlyCollection<Domain.Aggregates.Post>> GetPostsByCompanyId(Guid companyId);
+        Task<IReadOnlyCollection<Domain.Aggregates.Post>> GetPostsByUsername(string username);
+        Task<IReadOnlyCollection<Domain.Aggregates.Post>> GetPostsByHashtagId(Guid hashtagId);
+        Task<Domain.Aggregates.Post> GetPostById(Guid id);
+    }
+}
