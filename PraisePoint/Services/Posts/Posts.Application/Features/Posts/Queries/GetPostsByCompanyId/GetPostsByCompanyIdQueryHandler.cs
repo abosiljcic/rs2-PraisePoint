312b81f9e4fc816057b6b4a52f04a70f18191e91
nickla <ninalazarevic37@icloud.com>
2024-05-12 22:41:53 +0200

    New: Implemented Post queries

---
+++ PraisePoint/Services/Posts/Posts.Application/Features/Posts/Queries/GetPostsByCompanyId/GetPostsByCompanyIdQueryHandler.cs
@@ -0,0 +1,31 @@
+﻿using MediatR;
+using Posts.Application.Contracts.Factories;
+using Posts.Application.Contracts.Persistence;
+using Posts.Application.Features.Posts.Queries.GetPostById;
+using Posts.Application.Features.Posts.Queries.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Posts.Application.Features.Posts.Queries.GetPostsByCompanyId
+{
+    public class GetPostsByCompanyIdQueryHandler : IRequestHandler<GetPostsByCompanyIdQuery, List<PostViewModel>>
+    {
+        private readonly IPostRepository _repository;
+        private readonly IPostViewModelFactory _factory;
+
+        public GetPostsByCompanyIdQueryHandler(IPostRepository repository, IPostViewModelFactory factory)
+        {
+            _repository = repository ?? throw new ArgumentNullException(nameof(repository));
+            _factory = factory ?? throw new ArgumentNullException(nameof(factory));
+        }
+
+        public async Task<List<PostViewModel>> Handle(GetPostsByCompanyIdQuery request, CancellationToken cancellationToken)
+        {
+            var posts = await _repository.GetPostsByCompanyId(request.CompanyId);
+            return posts.Select(p => _factory.CreateViewModel(p)).ToList();
+        }
+    }
+}
---
+++ PraisePoint/Services/Posts/Posts.Application/Features/Posts/Queries/GetPostsByHashtagId/GetPostsByHashtagIdQuery.cs
@@ -0,0 +1,21 @@
+﻿using MediatR;
+using Posts.Application.Features.Posts.Queries.ViewModels;
+using Posts.Domain.Aggregates;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Posts.Application.Features.Posts.Queries.GetPostsByHashtagId
+{
+    public class GetPostsByHashtagIdQuery : IRequest<List<PostViewModel>>
+    {
+        public Guid HashtagId { get; set; }
+
+        public GetPostsByHashtagIdQuery(Guid hashtagId)
+        {
+            HashtagId = hashtagId;
+        }
+    }
+}
