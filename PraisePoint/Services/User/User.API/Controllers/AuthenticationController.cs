2fa4d851bcd59f74b259f6ba97d7966d843c7c15
Andrijana <andrijanab2000@gmail.com>
2024-05-14 20:30:57 +0200

    New: Added user registration

---
+++ PraisePoint/Services/User/User.API/Controllers/AuthenticationController.cs
@@ -0,0 +1,39 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Data;
+using User.API.Controllers.Base;
+using User.API.DTOs;
+
+namespace User.API.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class AuthenticationController : RegistrationControllerBase
+    {
+       // private readonly IAuthenticationService _authService;
+
+        public AuthenticationController(ILogger<AuthenticationController> logger, IMapper mapper, UserManager<Entities.User> userManager, RoleManager<IdentityRole> roleManager, IAuthenticationService authService)
+            : base(logger, mapper, userManager, roleManager)
+        {
+          //  _authService = authService ?? throw new ArgumentNullException(nameof(authService));
+        }
+
+        [HttpPost("[action]")]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<IActionResult> RegisterEmployee([FromBody] NewUserDto newUser)
+        {
+            return await RegisterNewUserWithRoles(newUser, new string[] { "Employee" });
+        }
+
+        [HttpPost("[action]")]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<IActionResult> RegisterAdmin([FromBody] NewUserDto newUser)
+        {
+            return await RegisterNewUserWithRoles(newUser, new string[] { "Admin" });
+        }
+    }
+}
--- PraisePoint/Services/User/User.API/Entities/User.cs
+++ PraisePoint/Services/User/User.API/Entities/User.cs
@@ -6,7 +6,6 @@ namespace User.API.Entities
     {
         public string FirstName { get; set; }
         public string LastName { get; set; }
-        public string Role { get; set; }
         public Guid CompanyId { get; set; }
         public string ImageUrl { get; set; }
         public bool Active { get; set; }
