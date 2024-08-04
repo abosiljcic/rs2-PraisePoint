f2732c528bc12cb4ec4a34caab844406153907b1
Nikol00 <mi19049@alas.matf.bg.ac.rs>
2024-08-04 23:11:42 +0200

    Update: Add dependency injection in Posts.API

--- PraisePoint/Services/Posts/Posts.API/Program.cs
+++ PraisePoint/Services/Posts/Posts.API/Program.cs
@@ -1,3 +1,8 @@
+using Posts.API.Extensions;
+using Posts.Application;
+using Posts.Infrastructure;
+using Posts.Infrastructure.Persistence;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +11,15 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddApplicationServices();
+builder.Services.AddInfrastructureServices(builder.Configuration);
+
+builder.MigrateDatabase<PostContext>((context, services) =>
+{
+    var logger = services.GetRequiredService<ILogger<PostContextSeed>>();
+    PostContextSeed.SeedAsync(context, logger).Wait();
+});
 
 var app = builder.Build();
 
