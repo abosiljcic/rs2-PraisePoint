1c1b6960834b391d58c8904e0cacd0498d58618e
Nikol00 <mi19049@alas.matf.bg.ac.rs>
2024-08-04 22:59:43 +0200

    New: Add factory for PostViewModel class

--- PraisePoint/Services/Posts/Posts.Infrastructure/Factories/PostViewModelFactory.cs
+++ PraisePoint/Services/Posts/Posts.Infrastructure/Factories/PostViewModelFactory.cs
@@ -31,7 +31,6 @@ public class PostViewModelFactory: IPostViewModelFactory
             var likeItem = new LikeViewModel();
             likeItem.Id = like.Id;
             likeItem.Username = like.Username;
-            likeItem.PostId = like.PostId;
             postLikes.Add(likeItem);
         }
 
