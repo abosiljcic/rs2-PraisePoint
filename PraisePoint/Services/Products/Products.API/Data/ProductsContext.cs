b7de401401162ee2796992c1f2711e6f7e97a9b9
Nikol00 <mi19049@alas.matf.bg.ac.rs>
2024-08-12 14:38:50 +0200

    New: Seeding products data

---
+++ PraisePoint/Services/Products/Products.API/Data/ProductsContext.cs
@@ -0,0 +1,18 @@
+﻿using MongoDB.Driver;
+using Products.API.Entities;
+
+namespace Products.API.Data
+{
+    public class ProductsContext : IProductsContext
+    {
+        public ProductsContext() 
+        { 
+            var client = new MongoClient("mongodb://localhost:27017");
+            var database = client.GetDatabase("ProductsDB");
+
+            Products = database.GetCollection<Product>("Products");
+            ProductsContextSeed.SeedData(Products);
+        }
+        public IMongoCollection<Product> Products { get; }
+    }
+}
---
+++ PraisePoint/Services/Products/Products.API/Entities/Product.cs
@@ -0,0 +1,10 @@
+﻿namespace Products.API.Entities
+{
+    public class Product
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string ImageUrl { get; set; }
+        public decimal Price { get; set; }
+    }
+}
