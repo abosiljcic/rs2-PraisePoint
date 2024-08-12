3c39796b2fc34f173159ff5b683e88236f0a5ac3
Andrijana <andrijanab2000@gmail.com>
2024-08-12 14:13:53 +0200

    New: Added getting user by username, getting all users by companyId

---
+++ PraisePoint/Services/User/User.API/Controllers/UserController.cs
@@ -0,0 +1,51 @@
+﻿using AutoMapper;
+using User.API.DTOs;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace User.API.Controllers
+{
+
+    [Authorize]
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly UserManager<Entities.User> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+        private readonly IMapper _mapper;
+
+        public UserController(UserManager<Entities.User> userManager, RoleManager<IdentityRole> roleManager, IMapper mapper)
+        {
+            _userManager = userManager ?? throw new ArgumentNullException(nameof(userManager));
+            _roleManager = roleManager ?? throw new ArgumentNullException(nameof(roleManager));
+            _mapper = mapper ?? throw new ArgumentNullException(nameof(mapper));
+        }
+
+        [Authorize(Roles = "Admin")]
+        [HttpGet("users/{companyId}")]
+        [ProducesResponseType(typeof(IEnumerable<UserDetailsDto>), StatusCodes.Status200OK)]
+        public async Task<ActionResult<IEnumerable<UserDetailsDto>>> GetAllUsersByCompany(Guid companyId)
+        {
+            var users = await _userManager.Users.Where(u => u.CompanyId == companyId).ToListAsync();
+
+            if (users == null || users.Count == 0)
+            {
+                return NotFound();
+            }
+
+            return Ok(users);
+        }
+
+        [Authorize(Roles = "Admin,Employee")]
+        [HttpGet("{username}")]
+        [ProducesResponseType(typeof(UserDetailsDto), StatusCodes.Status200OK)]
+        public async Task<ActionResult<UserDetailsDto>> GetUser(string username)
+        {
+            var user = await _userManager.Users.FirstOrDefaultAsync(user => user.UserName == username);
+            return Ok(_mapper.Map<UserDetailsDto>(user));
+        }
+    }
+}
---
+++ PraisePoint/Services/User/User.API/DTOs/UserDetailsDto.cs
@@ -0,0 +1,14 @@
+﻿namespace User.API.DTOs
+{
+    public class UserDetailsDto
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public Guid CompanyId { get; set; }
+        public string ImageUrl { get; set; }
+        public string Email { get; set; }
+        public string UserName { get; set; }
+        public string PhoneNumber { get; set; }
+
+    }
+}
