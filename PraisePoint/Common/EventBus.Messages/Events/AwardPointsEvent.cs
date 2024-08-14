c0ea9666446b4d9643f55f44bc14fa717c8a6049
KatMilosevic <milosevickatarina626@gmail.com>
2024-08-14 20:32:24 +0200

    Add AutoMapper for mapping UpdatePointsCommand and AwardPointsEvent

--- PraisePoint/Common/EventBus.Messages/Events/AwardPointsEvent.cs
+++ PraisePoint/Common/EventBus.Messages/Events/AwardPointsEvent.cs
@@ -2,9 +2,10 @@
 {
     public class AwardPointsEvent : IntegrationBaseEvent
     {
-        public string SenderUsername { get; set; }
-        public string ReceiverUsername { get; set; }
         //public Guid CompanyId { get; set; } ovo samo u slucaju da nisu jedinstveni usernameovi (to andrijana treba da podesi)
-        public int Points { get;  set; }
+
+        public string SenderId { get; set; }
+        public string ReceiverId { get; set; }
+        public int Points { get; set; }
     }
 }
