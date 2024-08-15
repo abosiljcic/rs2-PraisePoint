13d516281dc7af39db0cf6c6a2b65389da59ddc6
KatMilosevic <milosevickatarina626@gmail.com>
2024-08-15 16:27:13 +0200

    Add IBasketRepository interface and BasketRepository implementation using IDistributedCache

---
+++ PraisePoint/Services/Basket/Basket.API/Repositories/IBasketRepository.cs
@@ -0,0 +1,14 @@
+﻿using Basket.API.Entities;
+
+namespace Basket.API.Repositories
+{
+    //key - username
+    //value - sadrzaj korpe
+    public interface IBasketRepository
+    {
+        Task<ShoppingCart> GetBasket(string username);
+        Task<ShoppingCart> UpdateBasket(ShoppingCart basket);
+        Task DeleteBasket (string username);
+        Task<int> RemoveItemFromBasket(string username, string itemId, int quantityToRemove);
+    }
+}
